c1616c91dc86ccc5a0f93bd78c4a7d6f47ea541f
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2025-01-31 17:17:09 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_instance" "one" {
+count = 5
+ami = "ami-0ddc798b3f1a5117e"
+instance_type = "t2.medium"
+key_name = "swikp"
+vpc_security_group_ids = ["sg-05f044979e305302e"]
+tags = {
+Name = var.instance_names[count.index]
+}
+}
+
+variable "instance_names" {
+default = ["jenkins", "nexus", "AppServer-1", "AppServer-2", "Monitoring server"]
+}
