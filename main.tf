85e3ebbabb02d97274374137e1128dc215afb5da
syedmuzammil797 <114717197+syedmuzammil797@users.noreply.github.com>
2025-03-18 17:07:25 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-region = "us-east-1"
+region = "us-east-2"
 }
 
 resource "aws_instance" "one" {
 count = 5
-ami = "ami-0ddc798b3f1a5117e"
+ami = "ami-0cb91c7de36eed2cb"
 instance_type = "t2.medium"
-key_name = "swikp"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+key_name = "Fresh-Terra"
+vpc_security_group_ids = ["sg-0080b0cba614ff114"]
 tags = {
 Name = var.instance_names[count.index]
 }
